e382fc7393d5916310e042e89c93dd93cedafcdd
Jae-Keanu Phinisee <phiniseejae@gmail.com>
2025-10-15 14:40:36 -0500

    Changed db to local db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
-import { MongoClient, ServerApiVersion } from "mongodb";
 
-export const connectDB = async () => { 
-    try { 
-        const conn = await mongoose.connect(process.env.MONGO.URI);
-        console.log("MongoDB Connected:" );
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (err) {
+        console.error(`Error: ${err.message}`);
+        process.exit(1);
     }
-}
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
 import authRoutes from "./routes/authRoute.js";
 import dotenv from "dotenv"; 
-import mongoose from "mongoose";
 import express from "express"; 
 import { protect } from "./middleware/authMiddleware.js";
+import { connectDB } from "./config/db.js";
 
 dotenv.config(); 
 
@@ -11,15 +11,12 @@ const app = express();
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+connectDB();
 
 // Use routes
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5001; 
-
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on Port: ${PORT}`); 
 });
